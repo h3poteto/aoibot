e2ef89d4f15316b4c78ee268d26fe3adeafb9391
h3poteto <h3.poteto@gmail.com>
2014-01-21 01:11:24 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aoibot::Application.config.secret_token = '3eb7838f86b2888cfc6ddda346bcfbfbffb0d274758ae379ddbe3da08f5779f8842d63b4ab0cf1b3df85fafb95fb46c5de787e4b9cba6f465c1da754999d379c'
